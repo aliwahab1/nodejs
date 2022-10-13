55f4d5c72fd477b89c779c34262972564582584e
aliwahab <youssufsany@gmail.com>
2022-10-13 16:13:42 +0100

    Goals controller and routes setup

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const { set } = require('mongoose')
+const router = express.Router()
+const {
+    getGoals, 
+    setGoal, 
+    updateGoal, 
+    deleteGoal,
+} = require('../controllers/goalControler')
+
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
+
+
+
+// router.get('/', (req, res) => {
+//     res.status(200).json({message: 'Get goals'})
+// })
+
+// router.get('/', getGoals)
+
+// router.post('/', (req, res) => {
+//     res.status(200).json({message: 'Set goal'})
+// })
+
+// router.post('/', setGoal)
+
+// router.put('/:id', (req, res) => {
+//     res.status(200).json({message: `Update goal ${req.params.id}` })
+// })
+
+// router.put('/:id', updateGoal)
+
+// router.delete('/:id', (req, res) => {
+//     res.status(200).json({message: `Delete goal ${req.params.id}`})
+// })
+
+// router.delete('/:id', deleteGoal)
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+// add a middlewae function start
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false }))
+
+// add a middlewae function start
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
